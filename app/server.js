f4a5c7fe738ff81e1962bf2f31690cd58d093191
Anshul Goel <anshul@zenabi.com>
2019-11-13 17:33:12 -0500

    audit fixes

--- app/server.js
+++ app/server.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const responseTime = require('response-time');
-require('dotenv').config();
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const responseTime = require("response-time");
+require("dotenv").config();
 
 // Default node environment if the environment variable is missing
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-const logger = require('./lib/logger');
-const requestLogger = require('./lib/logger/request-logger');
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+const logger = require("./lib/logger");
+const requestLogger = require("./lib/logger/request-logger");
 
 const app = express();
 
-app.enable('trust proxy');
+app.enable("trust proxy");
 
 // Secure express app
 app.use(helmet());
-app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+app.use(helmet.referrerPolicy({ policy: "same-origin" }));
 // Enable All CORS Requests
 app.use(cors());
 // Add response time in headers
@@ -28,11 +28,14 @@ app.use(requestLogger);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.status(200).send('Application Started!').end();
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send("Application Started!")
+    .end();
 });
 
-require('./routes')(app);
+require("./routes")(app);
 
 const PORT = process.env.PORT || 7001;
 app.listen(PORT, () => {
@@ -41,10 +44,10 @@ app.listen(PORT, () => {
 
 // catch 404 and forward to error handler
 app.use((req, res) => {
-  res.setHeader('User', 'Invalid');
+  res.setHeader("User", "Invalid");
   res.status(404).send({
     code: 404,
-    description: 'Requested path not found',
+    description: "Requested path not found"
   });
 });
 
